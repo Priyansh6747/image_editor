439af8ac0c5487e5d23b42013bb8b8a96789743c
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2025-01-19 02:12:08 +0530

    Modifies UI for form

--- src/Components/Imageform.jsx
+++ src/Components/Imageform.jsx
@@ -1,7 +1,16 @@
 ﻿import PropTypes from "prop-types";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import InputFile from "./InputFile.jsx"
+import Button from "./Button.jsx"
+
 function ImageForm(props){
     const [ImgData, setImgData] = useState(null);
+    const [msg, setMsg] = useState("Click To Upload Image");
+    
+    useEffect(() => {
+        setMsg(ImgData? ImgData.name :"Click To Upload Image");
+    },[ImgData])
+    
     function handleChange(e){
         setImgData(e.target.files[0]);
     }
@@ -11,15 +20,33 @@ function ImageForm(props){
         props.setIsImage(true);
         props.setImg(ImgData);
     }
+    
+    
     return (
         <form onSubmit={handleSubmit}>
-            <input type={"file"}  onChange = {handleChange}  accept="image/*"/>
-            <button type="submit">Submit</button>
+            <div style={Style.container}>
+                <InputFile onValueChange={handleChange}/>
+                <p>{msg}</p>
+                <Button type={"submit"} text={"Confirm"} />
+            </div>
         </form>
     )
 }
+
 export default ImageForm;
 ImageForm.propTypes = {
     setIsImage: PropTypes.func.isRequired,
     setImg: PropTypes.func.isRequired,
+}
+
+const Style = {
+    container: {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap : '20px',
+    }
 }
